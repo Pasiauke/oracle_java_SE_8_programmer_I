8e4cb4eb246bacc836777ff3050c02faf48a3103
Paweł Kołczyk-Kasprzycki <pawel.kolczyk.kasprzycki@gmail.com>
2021-04-16 17:54:20 +0200

    Add some placeholders. Update tutorials versions to a correct one

---
+++ DataTypes/src/main/java/DataTypes.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class DataTypes {
+
+    public static void main(final String... args) {
+        if (args.length != 0) {
+            ARGS argument = ARGS.getArg(args[0]);
+            switch (argument) {
+                case VARIABLES:
+                    System.out.println("Hello! Start Variables tutorial...");
+                    break;
+                case PRIMITIVES:
+                    System.out.println("Hello! Start Primitives tutorial...");
+                    break;
+                case WRITE_OBJECTS:
+                    System.out.println("Hello! Start Write objects to field tutorial...");
+                    break;
+                case LIFECYCLE:
+                    System.out.println("Hello! Start Lifecycle tutorial...");
+                    break;
+                case WRAPPERS:
+                    System.out.println("Hello! Start Wrappers tutorial...");
+                    break;
+                default:
+                    System.out.println("Please use correct argument!");
+
+            }
+        }
+    }
+
+    private enum ARGS {
+        VARIABLES("variables"),
+        PRIMITIVES("primitives"),
+        WRITE_OBJECTS("writeObjects"),
+        LIFECYCLE("lifecycle"),
+        WRAPPERS("wrappers");
+
+        private final String name;
+
+        ARGS(final String name) {
+            this.name = name;
+        }
+
+        public static ARGS getArg(final String name) {
+            return Arrays.stream(values())
+                    .filter(a -> a.getName().equals(name))
+                    .findFirst().orElse(null);
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+    }
+}
